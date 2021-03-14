21f4c866843b10deefb4a26f65e204567406889a
Tayfun Kuşçu <69037566+tayfunkscu@users.noreply.github.com>
2021-03-14 16:52:31 +0300

    new files

--- WebScraping/pages/views.py
+++ WebScraping/pages/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from bs4 import BeautifulSoup
+import requests
+import re
+import string
 
 # Create your views here.
+"""
+checklist
++ url'yi al ve sonuç sayfasına yazdır
++ url'nin içeriğini ayıkla ve sonuç sayfasına yazdır
+- p etiketlerinin içeriklerini kelimelere ayırkan bir fonksiyon yaz
+- kelimelerin frekansını hesaplayan bir fonksiyon yaz
+"""
 
 
 def index(request):
@@ -14,11 +25,32 @@ def frekans(request):
 
 def frekansResult(request):
     url = request.POST.get("quantity")
-    context = {"url": url}
-    print(url)
+    allContent = scrapeUrl(url)
+    wordList = splitWords(allContent)
+    context = {"allContent": wordList}
     return render(request, "pages/frekansResult.html", context)
 
 
 """
-formdan input alma işlemleri tamam, beautiful soup indirilip birinci isterden yola çıkılacak
+Verilen url'deki p etiketlerinin içeriklerine sahip bir string listesi döndürür
 """
+
+
+def scrapeUrl(url):
+    r = requests.get(url)
+    source = BeautifulSoup(r.content, "lxml")
+    allContent = source.find_all("p")
+    allContentList = list()
+    for tag in allContent:
+        allContentList.append(tag.text)
+    return allContentList
+
+
+def splitWords(allContentList):
+    wordList = []
+    for tag in allContentList:
+        tag = tag.lower()
+        tag_no_punctuation = re.sub("[^\w\s]", "", tag)
+        tag_no_punctuation = tag_no_punctuation.split()
+        wordList.append(tag_no_punctuation)
+    return wordList
