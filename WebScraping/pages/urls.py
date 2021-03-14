89dcde25674891506dd03c05b3c32f5d46d68a8b
Tayfun Kuşçu <69037566+tayfunkscu@users.noreply.github.com>
2021-03-14 19:38:59 +0300

    new files

--- WebScraping/pages/urls.py
+++ WebScraping/pages/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("frekans", views.frekans, name="frekans"),
     path("frekansResult", views.frekansResult, name="frekansResult"),
+    path("keyword", views.keyword, name="keyword"),
+    path("keywordResult", views.keywordResult, name="keywordResult"),
 ]
--- WebScraping/pages/views.py
+++ WebScraping/pages/views.py
@@ -11,7 +11,7 @@ checklist
 + url'yi al ve sonuç sayfasına yazdır
 + url'nin içeriğini ayıkla ve sonuç sayfasına yazdır
 + p etiketlerinin içeriklerini kelimelere ayırkan bir fonksiyon yaz
-- kelimelerin frekansını hesaplayan bir fonksiyon yaz
++ kelimelerin frekansını hesaplayan bir fonksiyon yaz
 """
 
 
@@ -32,8 +32,23 @@ def frekansResult(request):
     return render(request, "pages/frekansResult.html", context)
 
 
+def keyword(request):
+    return render(request, "pages/keyword.html")
+
+
+def keywordResult(request):
+    url = request.POST.get("quantity")
+    allContent = scrapeUrl(url)
+    wordList = splitWords(allContent)
+    frequency = calculateFrequency(wordList)
+    top10 = sortFrequency(frequency)
+    context = {"words": top10.keys(), "frequency": top10.values()}
+    return render(request, "pages/keywordResult.html", context)
+
+
 """
 Verilen url'deki p etiketlerinin içeriklerine sahip bir string listesi döndürür
+///////////////////////////////////////////////////////////////////////////////
 """
 
 
@@ -66,3 +81,69 @@ def calculateFrequency(wordlist):
             num = frequency[word] + 1
             frequency.update({word: num})
     return frequency
+
+
+def sortFrequency(frequency):
+
+    baglacList = [
+        "zira",
+        "yoksa",
+        "yine",
+        "yeter",
+        "ki",
+        "yalnız",
+        "yahut",
+        "da",
+        "ya",
+        "veyahut",
+        "veya",
+        "ve",
+        "üstelik",
+        "öyleyse",
+        "öyle",
+        "oysaki",
+        "oysa",
+        "nitekim",
+        "ne",
+        "de",
+        "ne",
+        "yazık",
+        "var",
+        "nasıl",
+        "mademki",
+        "lâkin",
+        "kısacası",
+        "ise",
+        "ile",
+        "hem",
+        "hele",
+        "hatta",
+        "hâlbuki",
+        "gerek",
+        "gerekse",
+        "gene",
+        "fakat",
+        "demek",
+        "dahi",
+        "çünkü",
+        "bile",
+        "ancak",
+        "ama",
+        "açıkçası",
+    ]
+
+    frequency = dict(reversed(sorted(frequency.items(), key=lambda item: item[1])))
+
+    i = 0
+    top10 = {}
+    for key, value in frequency.items():
+        if i is 10:
+            break
+        else:
+            if key in baglacList:
+                continue
+            else:
+                top10[key] = value
+                i = i + 1
+
+    return top10
